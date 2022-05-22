b1ce675584c3d55e18a28659b9fe311da9343e01
KroKKy4 <91180337+KroKKy4@users.noreply.github.com>
2022-05-22 20:20:51 +0300

    Update euler.c

--- euler.c
+++ euler.c
@@ -4,7 +4,8 @@
 #include <math.h>
 #include <string.h>
 #define N "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
-int ex_1()
+
+int euler1()
 {
 	int sum = 0;
 
@@ -18,7 +19,7 @@ int ex_1()
 	return sum;
 }
 
-long long int ex_2()
+long long int euler2()
 {
 	long long int sum = 0;
 	long long int Fib1 = 0;
@@ -41,7 +42,7 @@ long long int ex_2()
 	return sum;
 }
 
-int prime(unsigned long long a)
+int Prime(unsigned long long a)
 {
 	unsigned long long i;
 	unsigned long long b = (unsigned long long)sqrt((double)a);
@@ -431,6 +432,100 @@ void euler15()
 	printf("15: %lli\n", arr[20][20]);
 }
 
+void Task_3()
+{
+	FILE* f;
+	fopen_s(&f, "date.txt", "r");
+
+	if (!f)
+	{
+		printf("file didn`t open\n");
+		return;
+	}
+
+	FILE* g;
+	fopen_s(&g, "date_new.txt", "w");
+
+	if (!g)
+		return;
+
+	int array[Date * 10];
+
+	for (int i = 0; i < Date * 10; ++i)
+	{
+		array[i] = getc(f);
+	}
+
+	for (int i = 0; i < Date * 10; ++i)
+	{
+		printf("%d ", array[i]);
+	}
+
+	int arr[Date][10];
+
+	for (int i = 0; i < Date; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+			arr[i][j] = 400;
+	}
+
+	int m = 0;
+
+	for (int i = 0; i < Date; ++i)
+	{
+		if (array[m] == 32)
+			m++;
+
+		for (int j = 0; j < 10; ++j)
+		{
+			if (array[m] == 48 || array[m] == 49 || array[m] == 50 || array[m] == 51 || array[m] == 52 || array[m] == 53 || array[m] == 54 || array[m] == 55 || array[m] == 56 || array[m] == 57)
+			{
+				arr[i][j] = array[m] - 48;
+				m++;
+			}
+			else
+			{
+				if (array[m] == 47)
+				{
+					arr[i][j] = 20;
+					m++;
+				}
+			}
+		}
+	}
+
+	int day[Date];
+
+	for (int i = 0; i < Date; ++i)
+	{
+		if (arr[i][1] != 400)
+		{
+			day[i] = (arr[i][6] * 1000 + arr[1][7] * 100 + arr[i][8] * 10 + arr[i][9]) * 10000 + (arr[i][3] * 10 + arr[i][4]) * 100 + arr[i][0] * 10 + arr[i][1];
+		}
+		else
+		{
+			for (int m = 0; m < 3; ++m)
+				day[i] = -1;
+		}
+	}
+
+	quickSort(day, 0, Date - 1);
+
+	for (int i = Date; i > 0; --i)
+	{
+		if (day[i] > 1000)
+		{
+			fprintf(g, "%d", day[i] % 100);
+			fpintf(g, "%c", '/');
+			day[i] /= 100;
+			fprintf(g, "%d", day[i] % 100);
+			fpintf(g, "%c", '/');
+			day[i] /= 100;
+			fprintf(g, "%d", day[i]);
+			fprintf(g, "%c", ' ');
+		}
+	}
+}
 
 
 int main() {
@@ -450,6 +545,7 @@ int main() {
 	euler13();
 	euler14();
 	euler15();
-
+	Task_3();
+	return 0 ;
 }
 
